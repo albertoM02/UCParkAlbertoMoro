887b1464e232ac961c7fca86f5f2d6b4f5f239fa
Alberto Moro <amc295@alumnos.unican.es>
2022-12-15 18:13:35 +0100

    Actualizado DenunciasDAO

--- P3/UCParkDAO/src/main/java/es/unican/ps/UCPark/daolayer/DenunciasDAO.java
+++ P3/UCParkDAO/src/main/java/es/unican/ps/UCPark/daolayer/DenunciasDAO.java
@@ -1,34 +1,48 @@
 package es.unican.ps.UCPark.daolayer;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import es.unican.ps.domain.Denuncia;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 
 public class DenunciasDAO implements IDenunciasDAOLocal, IDenunciasDAORemote {
 
+	
+	private EntityManager em;
+	
 	public Denuncia creaDenuncia(Denuncia d) {
-		
-		return null;
+		try {
+			em.persist(d);
+		} catch (EntityExistsException e) {
+			return null;
+		}
+		return d;
 	}
 
 	public Denuncia eliminaDenuncia(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		Denuncia denuncia = em.find(Denuncia.class, id);
+		if (denuncia != null) {
+			em.remove(denuncia);
+		} 
+		return denuncia;
 	}
 
 	public Denuncia actualizaDenuncia(Denuncia d) {
-		// TODO Auto-generated method stub
-		return null;
+		eliminaDenuncia(d.getID());		
+		return creaDenuncia(d);
 	}
 
 	public Denuncia denuncia(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		Denuncia denuncia = em.find(Denuncia.class, id);
+		return denuncia;
 	}
 
 	public Set<Denuncia> denuncias() {
-		// TODO Auto-generated method stub
-		return null;
+		 Query query = em.createQuery("SELECT d FROM Denuncias d");
+		 return (HashSet<Denuncia>) query.getResultList();
 	}
 
 }
